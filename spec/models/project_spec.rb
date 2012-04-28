20735af0ffa164b2d6c5edd176be0a08a1dde9af
Frank Münchmeyer <frank.muenchmeyer@adcloud.com>
2012-04-28 16:20:59 +0200

    added validations and tests for products

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
+require 'project'
 
 describe Project do
-  pending "add some examples to (or delete) #{__FILE__}"
+    describe "validation" do
+        it "should fail for empty titles" do
+             project = Project.new(
+                title: nil,
+            ).should_not be_valid
+        end
+        it "should fail for empty description" do
+             project = Project.new(
+                title: 'bla',
+                description: nil,
+            ).should_not be_valid
+        end
+    end
 end
