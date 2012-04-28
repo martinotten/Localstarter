c06b7d1a8357f46db9e1f8374a7c0994fc08b021
Martin Otten <martin@xince.de>
2012-04-28 17:39:49 +0200

    added index action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def user_location
+    session[:user_location] ||= request.location
+    @user_location ||= session[:user_location]
+    logger.debug session
+  end
 end
