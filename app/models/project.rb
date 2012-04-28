8ded13184f4df4ef135a8a6564416920d11c8945
Jan Pieper <jan.pieper@adcloud.com>
2012-04-28 14:54:55 +0200

    category belongs to project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
-  attr_accessible :address, :amount, :description, :geo_lat, :geo_lng, :title, :user, :valid_until
+  attr_accessible :address, :amount, :description, :geo_lat, :geo_lng, :title, :user, :category, :valid_until
   belongs_to :user
+  belongs_to :category
 end
