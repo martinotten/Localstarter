14a12b90a73631c392082d8666e5b1779bd4fd29
Martin Otten <martin@xince.de>
2012-04-28 16:46:42 +0200

    routed root url to projects#index

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Localstarter::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'projects#index'
 
   # See how all your routes lay out with "rake routes"
 
