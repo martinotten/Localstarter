c0c6c8ec8cd2da08e6f890e9f4cd754d03772741
Martin Otten <martin@xince.de>
2012-04-28 16:44:15 +0200

    changed session storrage to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120428122823) do
+ActiveRecord::Schema.define(:version => 20120428143253) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
-ActiveRecord::Schema.define(:version => 20120428133503) do
 
   create_table "projects", :force => true do |t|
     t.integer  "user_id"
@@ -33,6 +32,16 @@ ActiveRecord::Schema.define(:version => 20120428133503) do
     t.datetime "updated_at",                                  :null => false
     t.integer  "category_id"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "name"
