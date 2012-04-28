c13621c38bcfa5ac21e71062f6f24e4a774c711f
Martin Otten <martin@xince.de>
2012-04-28 14:32:54 +0200

    added validation for user.name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+  # TODO not needed if we use Facebook for login
+  validates :name, :presence => true, :uniqueness => true, :length => {minimum: 3, maximum: 25}
 end
