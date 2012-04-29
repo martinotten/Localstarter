961d08490193034f589ecf653fb1a500fb9d4584
Martin Otten <martin@xince.de>
2012-04-29 16:10:26 +0200

    activated geoip for remote addresses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def user_location
-    return [50.9800403, 6.847598199999999]
     location = {}
     if request.remote_ip === '127.0.0.1'
-      location = [50.9800403, 6.847598199999999]
+      return [50.9800403, 6.847598199999999]
     else
       location = request.location
       location = [location.latitude, location.longitude]
--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,7 @@ class Project < ActiveRecord::Base
   validate :description, presence: true, length: {minimum: 200, maximum: 10_000}
   validate :summary, presence: true, length: {minimum: 20, maximum: 200}
   validate :title, presence: true, length: {minimum: 20, maximum: 150} 
+  validate :valid_until, presence: true
   
   attr_accessible :address, :amount, :description, :latitude, :longitude, :title, :user, :category, :valid_until
   geocoded_by :address
