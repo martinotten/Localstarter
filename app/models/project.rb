ca5bc79dee9128bbcc86b40425d87b955fec05d1
Jan Pieper <jan.pieper@adcloud.com>
2012-04-29 12:30:02 +0200

    added current back status for a project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,13 @@
-
 class Project < ActiveRecord::Base
   validate :address, :present => true, :length => {minimum: 5, maximum: 255}
-  
+
   attr_accessible :address, :amount, :description, :latitude, :longitude, :title, :user, :category, :valid_until
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
-  
+
   belongs_to :user
   belongs_to :category
   validates :title, :description, :presence => true
+
+  has_many :backs
 end
