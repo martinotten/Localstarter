bca8cb051c4c86b90d9d0aa14bb257bfe692e428
Jan Pieper <jan.pieper@adcloud.com>
2012-04-29 10:55:15 +0200

    added functionality to edit a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,6 +7,19 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(params[:project])
+      redirect_to @project
+    else
+      render :action => "edit"
+    end
+  end
+
   def new
     @project = Project.new
   end
